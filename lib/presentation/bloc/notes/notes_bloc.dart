1a295dde813ece80925dbefcde259d474867a0fa
gershomlapaix <nsengiyumv33@gmail.com>
2025-07-05 13:14:23 +0200

    feat: add notes block

---
+++ lib/presentation/bloc/notes/notes_bloc.dart
@@ -0,0 +1,76 @@
+mport 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/data/repositories/note_repository.dart';
+import 'package:notes_app/presentation/bloc/notes/notes_event.dart';
+import 'package:notes_app/presentation/bloc/notes/notes_state.dart';
+
+class NotesBloc extends Bloc<NotesEvent, NotesState> {
+  final NoteRepository _noteRepository;
+
+  NotesBloc(this._noteRepository) : super(NotesInitial()) {
+    on<FetchNotes>(_onFetchNotes);
+    on<AddNote>(_onAddNote);
+    on<UpdateNote>(_onUpdateNote);
+    on<DeleteNote>(_onDeleteNote);
+  }
+
+  Future<void> _onFetchNotes(
+      FetchNotes event,
+      Emitter<NotesState> emit,
+      ) async {
+    emit(NotesLoading());
+    try {
+      final notes = await _noteRepository.fetchNotes();
+      emit(NotesLoaded(notes: notes));
+    } catch (e) {
+      emit(NotesError(message: e.toString()));
+    }
+  }
+
+  Future<void> _onAddNote(
+      AddNote event,
+      Emitter<NotesState> emit,
+      ) async {
+    try {
+      await _noteRepository.addNote(event.title, event.content);
+      final notes = await _noteRepository.fetchNotes();
+      emit(NotesOperationSuccess(
+        message: 'Note added successfully',
+        notes: notes,
+      ));
+    } catch (e) {
+      emit(NotesError(message: e.toString()));
+    }
+  }
+
+  Future<void> _onUpdateNote(
+      UpdateNote event,
+      Emitter<NotesState> emit,
+      ) async {
+    try {
+      await _noteRepository.updateNote(event.id, event.title, event.content);
+      final notes = await _noteRepository.fetchNotes();
+      emit(NotesOperationSuccess(
+        message: 'Note updated successfully',
+        notes: notes,
+      ));
+    } catch (e) {
+      emit(NotesError(message: e.toString()));
+    }
+  }
+
+  Future<void> _onDeleteNote(
+      DeleteNote event,
+      Emitter<NotesState> emit,
+      ) async {
+    try {
+      await _noteRepository.deleteNote(event.id);
+      final notes = await _noteRepository.fetchNotes();
+      emit(NotesOperationSuccess(
+        message: 'Note deleted successfully',
+        notes: notes,
+      ));
+    } catch (e) {
+      emit(NotesError(message: e.toString()));
+    }
+  }
+}
